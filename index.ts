2e21086f40e455749474eb9cfb3b94ae9a315a17
Thomas Van <tinbolw@gmail.com>
2026-03-24 21:38:55 -0700

    update tsconfig

--- index.ts
+++ index.ts
@@ -47,7 +47,7 @@ client.on(Events.InteractionCreate, async interaction => {
 	} 
 
 	const command = interaction.client.commands.get(interaction.commandName);
-
+	
 	if (!command) {
 		console.error(`No command matching ${interaction.commandName} was found.`);
 		return;
